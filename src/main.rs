bbc4c66e36ed3874b57704e58c0832c6930dec2d
ABigWalrus <morgjik@gmail.com>
2024-03-16 09:19:16 +0100

    implmented the bubble sort

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+// mod algo;
+// fn main() {
+//     let mut test_list = vec![1, 2, 3, 4, 5];
+//     test_list.push(1);
+//     algo::sorting::bubble_sort(&mut test_list);
+// }
+
+mod algo;
 fn main() {
-    println!("Hello, world!");
+    let mut test_list: [i32; 5] = [4, 3, 1, 4, 5];
+    println!("{:?}", test_list);
+    // algo::sorting::swap(&mut test_list, 0, 1);
+    algo::sorting::bubble_sort(&mut test_list);
+    println!("{:?}", test_list);
 }
