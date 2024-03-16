b6c59a658419bc043eb8f9985d7a2b3b58bd0d0c
ABigWalrus <morgjik@gmail.com>
2024-03-16 11:17:12 +0100

    implemented selection sort

--- src/algo.rs
+++ src/algo.rs
@@ -1,4 +1,45 @@
 pub mod sorting{
+
+    pub fn find_min<T: std::cmp::PartialOrd + Copy>(list: &[T]) -> T{
+        let mut min = list[0];
+        for i in 1..list.len(){
+            if list[i] < min{
+                min = list[i];
+            }
+        }
+        min
+    }
+
+    pub fn find_min_interval<T: std::cmp::PartialOrd + Copy>(list: &[T], from: usize, to: usize) -> usize{
+        let mut min = from;
+        for i in (from + 1)..to{
+            if list[i] < list[min]{
+                min = i;
+            }
+        }
+        min
+    }
+
+    pub fn find_max_interval<T: std::cmp::PartialOrd + Copy>(list: &[T], from: usize, to: usize) -> usize{
+        let mut max = from;
+        for i in (from + 1)..to{
+            if list[i] > list[max]{
+                max = i;
+            }
+        }
+        max
+    }
+
+    pub fn find_max<T: std::cmp::PartialOrd + Copy>(list: &[T]) -> T{
+        let mut max = list[0];
+        for i in 1..list.len(){
+            if list[i] > max{
+                max = list[i];
+            }
+        }
+        max
+    }
+
     pub fn is_sorted<T: std::cmp::PartialOrd + Copy>(list: &mut [T]) -> bool{
         for i in 0..(list.len() - 1){
             if list[i] > list[i + 1]{
@@ -21,6 +62,17 @@ pub mod sorting{
                 }
             }
         }
-        
+    }
+
+    pub fn selection_sort<T: std::cmp::PartialOrd + Copy>(list: &mut [T]){
+        let mut n_sorted = 0;
+        let mut n = list.len();
+        let mut max;
+        for i in 0..(list.len() - 1){
+            max = find_max_interval(list, 0, n - i);
+            let t = list[max];
+            list[max] = list[n - i - 1];
+            list[n - i - 1] = t;
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-// mod algo;
-// fn main() {
-//     let mut test_list = vec![1, 2, 3, 4, 5];
-//     test_list.push(1);
-//     algo::sorting::bubble_sort(&mut test_list);
-// }
-
 mod algo;
+
 fn main() {
-    let mut test_list: [i32; 5] = [4, 3, 1, 4, 5];
-    println!("{:?}", test_list);
-    println!("{}", algo::sorting::is_sorted(&mut test_list));
-    // algo::sorting::swap(&mut test_list, 0, 1);
-    algo::sorting::bubble_sort(&mut test_list);
-    println!("{:?}", test_list);
-    println!("{}", algo::sorting::is_sorted(&mut test_list));
+    let mut test_list1: [i32; 5] = [4, 3, 1, 4, 5];
+    let mut test_list2: [i32; 5] = [1, 2, 3, 4, 5];
+    let mut test_list3: [i32; 5] = [5, 4, 3, 2, 1];
+
+    println!("test_list1 {:?}", test_list1);
+    println!("test_list2 {:?}", test_list2);
+    println!("test_list3 {:?}", test_list3);
+    
+    algo::sorting::selection_sort(&mut test_list1);
+    algo::sorting::selection_sort(&mut test_list2);
+    algo::sorting::selection_sort(&mut test_list3);
+    
+    println!("test_list1 {:?}", test_list1);
+    println!("test_list2 {:?}", test_list2);
+    println!("test_list3 {:?}", test_list3);
 }
