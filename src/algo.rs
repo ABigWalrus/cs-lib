db6ae36c7682258ace75224c1ca6c027982e5e66
ABigWalrus <morgjik@gmail.com>
2024-03-16 09:27:02 +0100

    implemented sorting check

--- src/algo.rs
+++ src/algo.rs
@@ -1,31 +1,24 @@
-// pub mod sorting{
-//     pub fn bubble_sort(list: &mut Vec<i32>){
-//         println!("fist element: {}", list[0]);
-//         list[0] = 10;
-//         println!("fist element: {}", list[0]);
-//     }
-// }
+pub mod sorting{
+    pub fn is_sorted<T: std::cmp::PartialOrd + Copy>(list: &mut [T]) -> bool{
+        for i in 0..(list.len() - 1){
+            if list[i] > list[i + 1]{
+                return false;
+            }
+        }
+        true
+    }
 
-pub mod sorting{ 
-    // pub fn swap<T>(first: T, second_index: i32){
-    //     let mut temp = list[first_index];
-    //     list[first_index] = list[second_index];
-    //     list[second_index] = temp;
-    // }
     pub fn bubble_sort<T: std::cmp::PartialOrd + Copy>(list: &mut [T]){
         let mut flag = true;
         while flag{
             flag = false;
             for i in 0..(list.len() - 1){
                 if list[i] > list[i + 1] {
-                    // std::mem::swap(&mut list[i], &mut list[i + 1]);
                     let t = list[i];
                     list[i] = list[i + 1];
                     list[i + 1] = t;
                     flag = true;
-                    println!("I swapped");
                 }
-                println!("I did something");
             }
         }
         
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,9 @@ mod algo;
 fn main() {
     let mut test_list: [i32; 5] = [4, 3, 1, 4, 5];
     println!("{:?}", test_list);
+    println!("{}", algo::sorting::is_sorted(&mut test_list));
     // algo::sorting::swap(&mut test_list, 0, 1);
     algo::sorting::bubble_sort(&mut test_list);
     println!("{:?}", test_list);
+    println!("{}", algo::sorting::is_sorted(&mut test_list));
 }
