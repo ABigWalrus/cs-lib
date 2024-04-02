88e67a959b7de52d6da0b2b90bfa81bc4f83af8c
ABigWalrus <morgjik@gmail.com>
2024-04-02 23:37:50 +0200

    created integration tests dir

---
+++ tests/main_test.rs
@@ -0,0 +1,12 @@
+mod cs_lib::algo;
+
+#[test]
+fn sorting_test() {
+    let mut test_list1: [i32; 5] = [1, 2, 3, 4, 5];
+    let mut test_list2: [i32; 5] = [4, 3, 1, 4, 5];
+    let mut test_list3: [i32; 5] = [5, 2, 3, 2, 1];
+
+    assert!(algo::sorting::is_sorted(test_list1));
+    assert!(!algo::sorting::is_sorted(test_list2));
+    assert!(!algo::sorting::is_sorted(test_list3));
+}
